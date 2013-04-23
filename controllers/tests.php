fb3f3d377281e9c9db6f6aff250023d120846bac
kaia <kaia.konsap@khk.ee>
2013-04-23 15:52:43 +0300

    Fix bugs

--- controllers/tests.php
+++ controllers/tests.php
@@ -8,27 +8,27 @@ class tests
 
 	function index()
 	{
-
 		$this->scripts[] = 'tests.js';
 		global $request;
 		global $_user;
 		$tests = get_all("SELECT * FROM test NATURAL JOIN user WHERE test.deleted=0");
-		$id=$_SESSION['user_id'];
-		$status=get_one("SELECT status FROM user WHERE user_id='$id'");
+		$id = $_SESSION['user_id'];
+		$status = get_one("SELECT status FROM user WHERE user_id='$id'");
 		//merge master view which decides which body to put
 		require 'views/master_view.php';
 
 
 	}
+
 	function edit()
 	{
 		global $request;
-		$this->scripts[]='test_add_edit.js';
-		$id=$request->params[0];
-		$test=get_all("SELECT * FROM test WHERE test_id='$id'");
-		$question=get_all("SELECT question_text FROM question WHERE test_id='$id'");
-		$test=$test[0];
-		$question=isset($question[0])?$question[0]:array('question_text'=>'');
+		$this->scripts[] = 'test_add_edit.js';
+		$id = $request->params[0];
+		$test = get_all("SELECT * FROM test WHERE test_id='$id'");
+		$question = get_all("SELECT question_text FROM question WHERE test_id='$id'");
+		$test = $test[0];
+		$question = isset($question[0]) ? $question[0] : array('question_text' => '');
 		require 'views/master_view.php';
 
 	}
@@ -42,17 +42,19 @@ class tests
 		require 'views/master_view.php';
 	}
 
-	function add(){
+	function add()
+	{
 		global $request;
-		$this->scripts[]='test_add_edit.js';
-		$test=array(
-			'test_id'=>'',
-			'name'=>'',
-			'introduction'=>'',
-			'conclusion'=>'',
-			'passcode'=>'',
-			'question_text'=>''
+		$this->scripts[] = 'test_add_edit.js';
+		$test = array(
+			'test_id'       => '',
+			'name'          => '',
+			'introduction'  => '',
+			'conclusion'    => '',
+			'passcode'      => '',
+			'question_text' => ''
 		);
+		$question = array('question_text' => '');
 		require 'views/master_view.php';
 
 	}
